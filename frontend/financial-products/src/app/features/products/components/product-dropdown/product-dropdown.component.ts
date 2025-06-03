5f79c0bddb0b4eef620db3e7221b479a65951ea6
edisonzm9 <eazamoram99@gmail.com>
2025-06-03 11:27:19 -0500

    Feat(F5): creación de produc-dropdown

---
+++ frontend/financial-products/src/app/features/products/components/product-dropdown/product-dropdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-dropdown.component.html',
+  styleUrl: './product-dropdown.component.scss'
+})
+export class ProductDropdownComponent {
+
+}
